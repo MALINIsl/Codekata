67664e036831b24765515636b7f8e59e2551bfd0
MALINIsl <malushladi511@gmail.com>
2017-06-20 23:20:36 -0700

    Update and rename npz1.py to npz2.py

--- npz1.py
+++
@@ -1,7 +0,0 @@
-va = int(raw_input("The number entered is "))
-if (va  == 0):
-	print("Zero")
-elif (va < 0):
-	print("Negative")
-else:
-	print("Positive")
---
+++ npz2.py
@@ -0,0 +1,9 @@
+va1 = int(raw_input("The number ))
+if (va1  == 0):
+	print("entered is Zero")
+elif (va1 < 0):
+	print("entered is Negative")
+elsif (va1> 0):
+	print("entered is Positive")
+else:
+	print("Null")
