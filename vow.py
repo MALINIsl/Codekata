16476cd237b9254fab786deb851d44c0c4a195a1
MALINI S L <malushladi511@gmail.com>
2017-06-21 04:40:51 -0700

    Create vow.py

---
+++ vow.py
@@ -0,0 +1,7 @@
+l = raw_input("The alphabet given ")
+if (l in 'a' or l in 'e' or l in 'i' or l in 'o' or l in 'u'): 
+	print("is a vowel.")
+elif (l in 'A' or l in 'E' or l in 'I' or l in 'O' or l in 'U'):
+	print("is a vowel.")
+else:
+	print("is a consonant.") 
