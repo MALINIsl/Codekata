7a75ebccbcb5f52c56b1154f9456177cb0eb05d1
MALINI S L <malushladi511@gmail.com>
2017-06-21 04:41:48 -0700

    Create large.py

---
+++ large.py
@@ -0,0 +1,9 @@
+a = int(raw_input("The maximum among "))
+b = int(raw_input("the "))
+c = int(raw_input("three numbers are: "))
+if a > b and a > c :
+	print(a)
+elif b > a and b > c :
+	print(b)
+else:
+	print(c)
